5b7995f82f5a42b83862f3b1a318e321192a4ab1
root <root@ip-172-31-38-62.ap-south-1.compute.internal>
2023-09-05 07:51:16 +0000

    update the code

--- deployTheCode.sh
+++ deployTheCode.sh
@@ -8,27 +8,37 @@ aws_server_repo_path="/home/lokesh/CI-CD-Pipeline_Tool"
 nginx_html_path="/var/www/autodeploy"
 
 # Navigate to the local repository directory
-cd $aws_server_repo_path
+cd "$aws_server_repo_path"
 
-# Pull the latest changes from the GitHub repository
-git pull
+# Check if there are any unstaged changes
+if [ -n "$(git status --porcelain)" ]; then
+    # If there are unstaged changes, stash them
+    git stash
 
-# Check if index.html has changed
-if [[ $(git diff --name-only HEAD@{1} HEAD) =~ "index.html" ]]; then
-    # Copy the updated index.html to the Nginx server path
-    sudo cp index.html $nginx_html_path
-    
-    # Restart Nginx
-    sudo systemctl restart nginx
-    
-    echo "index.html updated, and Nginx restarted."
+    # Pull the latest changes from the GitHub repository
+    git pull
+
+    # Apply the stashed changes back to the working directory
+    git stash apply
+
+    # Check if index.html has changed
+    if [[ $(git diff --name-only HEAD@{1} HEAD) =~ "index.html" ]]; then
+        # Copy the updated index.html to the Nginx server path
+        sudo cp index.html "$nginx_html_path"
+        
+        # Restart Nginx
+        sudo systemctl restart nginx
+    fi
 else
-    echo "No changes detected in index.html."
+    # If there are no unstaged changes, simply pull and check for index.html changes
+    git pull
+
+    # Check if index.html has changed
+    if [[ $(git diff --name-only HEAD@{1} HEAD) =~ "index.html" ]]; then
+        # Copy the updated index.html to the Nginx server path
+        sudo cp index.html "$nginx_html_path"
+        
+        # Restart Nginx
+        sudo systemctl restart nginx
+    fi
 fi
-
-
-
-
-
-
-# -cron job = * * * * * /usr/bin/python3 /home/lokesh/CI-CD-Pipeline_Tool/CI-CD-PipeLine.py
